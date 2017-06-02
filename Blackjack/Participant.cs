37bd0357a02420572eeb9d747c967644f0b18b15
Abster2970 <ilya.krv2970@gmail.com>
2017-06-02 19:21:29 +0300

    Added participant.cs

---
+++ Blackjack/Participant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack
+{
+    abstract class Participant
+    {
+        public List<Card> Cards { get; set; }
+        public int Total { get; set; } = 0;
+        public string Name { get; set; }
+
+        public Participant(string name)
+        {
+            Cards = new List<Card>();
+            Name = name;
+        }
+
+        public void PrintCards()
+        {
+            Console.WriteLine($"\n#{Name} cards: ");
+            foreach (var card in Cards)
+            {
+                Console.WriteLine(card.ToString());
+            }
+        }
+    }
+}
