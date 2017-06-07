a9d2f46a4ef64bc12713e9fc23e6c43caabd8094
Abster297070 <ilya.krv2970@gmail.com>
2017-06-07 21:12:22 +0300

    Added deck and hand classes

---
+++ Blackjack/Deck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blackjack
+{
+    class Deck
+    {
+        private readonly List<Card> cards = new List<Card>(52);
+
+        public IReadOnlyCollection<Card> Cards
+        {
+            get { return cards.AsReadOnly(); }
+        }
+
+        public Deck()
+        {
+            this.Fill();
+        }
+        
+        public void Fill()
+        {
+            cards.Clear();
+            cards.AddRange(
+                Enumerable.Range(1, 4)
+                .SelectMany(s => Enumerable.Range(1, 13).Select(r => new Card((Suits)s, (Ranks)r))));
+        }
+
+        public void Shuffle()
+        {
+            var cardsCount = cards.Count;
+            var cardsToShuffle = cardsCount;
+            var rnd = new Random();
+
+            while(cardsToShuffle > 1)
+            {
+                var firstRndCardId = rnd.Next(0, cardsCount);
+                var secondRndCardId = rnd.Next(0, cardsCount);
+
+                var tmp = cards[firstRndCardId];
+                cards[firstRndCardId] = cards[secondRndCardId];
+                cards[secondRndCardId] = tmp;
+
+                cardsToShuffle--;
+            }
+        }
+        
+        public Card GetCard()
+        {
+            if (Cards.Count == 0) return null;
+            
+            var card = cards.First();
+            cards.Remove(card);
+
+            return card;
+        }
+    }
+}
---
+++ Blackjack/Hand.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blackjack
+{
+    class Hand
+    {
+        private readonly List<Card> cards = new List<Card>();
+
+        public bool IsDealer { get; private set; }
+
+        public IReadOnlyCollection<Card> Cards
+        {
+            get { return cards.AsReadOnly(); }
+        }
+
+        public int SoftValue
+        {
+            get
+            {
+                return cards.Select(c => (int)c.Rank > 1 && (int)c.Rank < 11 ? (int)c.Rank : (int)c.Rank == 1 ? 11 : 10).Sum();
+            }
+        }
+
+        public int TotalValue
+        {
+            get
+            {
+                var totalValue = SoftValue;
+                var aces = cards.Count(c => c.Rank == Ranks.Ace);
+
+                while (aces-- > 0 && totalValue > 21)
+                {
+                    totalValue -= 10;
+                }
+
+                return totalValue;
+            }
+        }
+        
+        public int FaceValue
+        {
+            get
+            {
+                var totalValue = cards.Where(c => c.IsFaceUp)
+                    .Select(c => (int)c.Rank > 1 && (int)c.Rank < 11 ? (int)c.Rank : (int)c.Rank == 1 ? 11 : 10).Sum();
+                var aces = cards.Count(c => c.Rank == Ranks.Ace);
+
+                while (aces-- > 0 && totalValue > 21)
+                {
+                    totalValue -= 10;
+                }
+
+                return totalValue;
+            }
+        }
+
+        public List<Card> VisibleCards
+        {
+            get
+            {
+                return Cards.Where(c => c.IsFaceUp).ToList();
+            }
+        }
+
+        public Hand(bool isDealer)
+        {
+            IsDealer = isDealer;
+        }
+
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+
+        public void Show()
+        {
+            foreach(var card in cards)
+            {
+                if (!card.IsFaceUp)
+                {
+                    card.Flip();
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            cards.Clear();
+        }
+    }
+}
